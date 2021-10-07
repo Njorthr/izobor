97e8b1490b4d1cd2f873549f3f7da573a610582c
Ahmet Melih Guven <ahmetmelihguven1999@gmail.com>
2021-10-07 13:43:15 +0300

    Initial Commit

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form
+$firma = $_POST['firma']  
+$name = $_POST['name'];
+$number = $_POST['phone'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "info@izobor.com";
+$subject = "Mail From İzobor.com";
+$txt ="Firma = ". $firma . "Ad Soyad = ". $name . "\r\n  Email = " . $email . "\r\n Mesaj =" . $message;
+$headers = "From: noreply@izobor.com" . "\r\n" .
+
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:tesekkur.html");
+?>
